aba4f9f9a885d55b6a6ef5ad5a10f86cd870dc0a
Ramazanovakrom0723 <akromramazanov77@gmail.com>
2024-03-30 01:32:41 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,122 @@
+// 1-misol
+
+// let arr = [1,2,3,4,5,6,7,8,9]
+// let arr_pop = arr.pop()
+// let arr_shift = arr.shift()
+// console.log(arr_pop + arr_shift)
+
+
+
+
+
+// 2-misol
+
+// let random_number = parseInt(Math.random() * 100)
+// let arr = [2,4,63,5,7,36] 
+// arr.unshift(random_number)
+// console.log(`${random_number} random raqam`)
+// if (random_number % 2 == 0) {
+//     arr.shift(random_number + 1)
+//     console.log(`${arr} Randomdan juft son ciqarilganda toq son qilingani`)
+// }else{
+//     arr.unshift(random_number + 1)
+//     console.log(`${arr} Randomdan toq son ciqarilganda juft son qilingani`)
+// }
+
+
+
+
+
+// 3-masala
+
+// let n =[]
+// let arr =  [5, 16, 7, 4, 10, 3, 4, 4, 14, 5, 14, 4, 44, 4]
+// for (let i = 0; i < arr.length; i++) {
+//    if (arr[i]!=4) {
+//     n.push(arr[i])
+//    } 
+    
+// }
+// console.log(n)
+
+
+
+
+
+// 4-masala
+
+// let arr =  [1,3,2,4,5,6,99,102]
+// arr.sort((a, b)=> a - b)
+// console.log(arr)
+//   for ( let i = 0; i < arr.length; i++){
+//     if ( arr[i] == 5){
+//         console.log("Osish tartibida " + i + "ga teng");
+//     }
+// }
+// arr.sort((a, b)=> b - a)
+//   for ( let i = 0; i < arr.length; i++){
+//     if ( arr[i] == 5){
+//         console.log("Kamayish tartibida " + i + "ga teng");
+//     }
+// }
+
+
+
+
+// 5-masala
+
+// 1-usul
+
+// let arr = [1,2,3,4,5,6,7,8,89]
+// let total = 0
+// for (let i = 0; i < arr.length; i++) {
+//     total += arr[i]
+// }
+// console.log(total)
+
+// 2-usul
+
+// let arr = [1,2,3,4,5,6,7,8,89]
+// let total = arr.reduce((a,b)=> a + b)
+// console.log(total)
+
+
+
+
+// 6-masala
+
+// function fn(target){
+// let arr =  [1,3,2,4,5,6,99,102]
+// arr.sort((a, b)=> a-b)
+// console.log(arr)
+//   for ( let i = 0; i < arr.length; i++){
+//     if ( arr[i] == target){
+//         console.log("Osish tartibida " + i + "ga teng");
+//     }
+// }
+// arr.sort((a, b)=> b - a)
+//   for ( let i = 0; i < arr.length; i++){
+//     if ( arr[i] == target){
+//         console.log("Kamayish tartibida " + i + "ga teng");
+//     }
+// }
+// }
+// fn(3)
+// fn(102)
+
+
+
+
+// 8-masala
+
+// let obj = {old: 19, school: 35}
+// function fn(target){
+    
+// }
+// fn()
+
+
+
+
+
+
